d195a93746f59e5dc1025aefd5a7e171f16570e9
Zadkiel M <zadkiel.m@gmail.com>
2017-01-09 13:06:48 -0500

    - Update Website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,24 @@
 /* Write here your custom javascript codes */
+$( "#home-menu" ).click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home-section").offset().top
+    }, 1500);
+});
+
+$( "#services-menu" ).click(function() {
+    $('html, body').animate({
+        scrollTop: $("#service-section").offset().top
+    }, 1500);
+});
+
+$( "#team-menu" ).click(function() {
+    $('html, body').animate({
+        scrollTop: $("#team-section").offset().top
+    }, 1500);
+});
+
+$( "#philosophy-menu" ).click(function() {
+    $('html, body').animate({
+        scrollTop: $("#philosophy-section").offset().top
+    }, 1500);
+});
